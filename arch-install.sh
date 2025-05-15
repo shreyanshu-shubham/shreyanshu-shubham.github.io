45b0e74d2b7265141554c29f3cfb77c1a8ec8ccb
shreyanshu-shubham <shreyanshu.dps@gmail.com>
2025-05-16 02:41:56 +0530

    arch-install script

--- arch-install.sh
+++ arch-install.sh
@@ -1,9 +1,6 @@
 setfont ter-114b
 
-echo -n "Enter the drive path: "
-read var_drive_path
-
-wipefs -a ${var_drive_path}
+wipefs -a /dev/sda
 
 (
 # EFI PART
@@ -15,24 +12,31 @@ echo +1G # Last sector (Accept default: varies)
 # boot PART
 echo n # Add a new partition
 echo   # Partition number
-echo   # First sector (Accept default: 1)
+echo   # First sector (Accept default: 2)
 echo +1G # Last sector (Accept default: varies)
+# swap PART
+echo n # Add a new partition
+echo   # Partition number
+echo   # First sector (Accept default: 3)
+echo +8G # Last sector (Accept default: varies)
 # root PART
 echo n # Add a new partition
 echo   # Partition number
-echo   # First sector (Accept default: 1)
+echo   # First sector (Accept default: 4)
 echo   # Last sector (Accept default: varies)
 
 echo w # Write changes
-) | fdisk ${var_drive_path}
+) | fdisk /dev/sda
 
-mkfs.fat -F 32 ${var_drive_path}1
-mkfs.ext4 ${var_drive_path}2
-mkfs.ext4 ${var_drive_path}3
+mkfs.fat -F 32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mkswap /dev/sda3
+mkfs.ext4 /dev/sda4
 
-mount ${var_drive_path}3 /mnt
-mount --mkdir ${var_drive_path}2 /mnt/boot
-mount --mkdir ${var_drive_path}1 /mnt/boot/EFI
+mount /dev/sda4 /mnt
+swapon /dev/sda3
+mount --mkdir /dev/sda2 /mnt/boot
+mount --mkdir /dev/sda1 /mnt/boot/EFI
 
 pacstrap -K /mnt base linux linux-firmware linux-headers
 genfstab -U /mnt >> /mnt/etc/fstab
