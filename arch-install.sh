c5820f2d31bfe85a7e855b5f1b4dcf3bfbeb3f80
sourav <sourabsethi66@gmail.com>
2025-05-16 01:32:26 +0530

    arch-install script

---
+++ arch-install.sh
@@ -0,0 +1,37 @@
+setfont ter-114b
+
+(
+# EFI PART
+echo g # Create a new empty DOS partition table
+echo n # Add a new partition
+echo   # Partition number
+echo   # First sector (Accept default: 1)
+echo +1G # Last sector (Accept default: varies)
+# boot PART
+echo g # Create a new empty DOS partition table
+echo n # Add a new partition
+echo   # Partition number
+echo   # First sector (Accept default: 1)
+echo +1G # Last sector (Accept default: varies)
+# boot PART
+echo g # Create a new empty DOS partition table
+echo n # Add a new partition
+echo   # Partition number
+echo   # First sector (Accept default: 1)
+echo   # Last sector (Accept default: varies)
+
+echo w # Write changes
+) | fdisk /dev/sda
+
+mkfs.fat -F 32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda3
+
+mount /dev/sda3 /mnt
+mount --mkdir /dev/sda2 /mnt/boot
+mount --mkdir /dev/sda1 /mnt/boot/EFI
+
+pacstrap -K /mnt base linux linux-firmware linux-headers
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
