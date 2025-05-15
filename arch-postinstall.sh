c79daa65a424417fac4ea969720475bae39a7e3c
shreyanshu-shubham <shreyanshu.dps@gmail.com>
2025-05-16 03:01:31 +0530

    arch-postinstall script

--- arch-postinstall.sh
+++ arch-postinstall.sh
@@ -5,34 +5,18 @@ passwd #change root password
 useradd -m -g users -G wheel shreyanshu
 passwd shreyanshu 
 
-pacman -S base-devel dosfstoold grub efibootmgr mtools nano networkmanager openssh sudo
-    os-prober # if dual boot
-    gnome gnome-tweaks # for gnome de
+pacman -S base-devel dosfstoold grub efibootmgr mtools vim networkmanager openssh sudo
 
-systemctl enable --now sshd
+systemctl enable sshd
 
 pacman -S linux linux-headers linux-firmware
 
-if intel/amd gpu use `lspci` to figure it out
-    pacman -S mesa
-for nvidia
-    pacman -S nvidia nvidia-utils
-
-if intel newer than including broadwell for hardware decoding
-    pacman -S intel-media-driver
-for amd hardware decoding
-    pacman -S libva-mesa-driver
-
-for virtual box (vmware)
-    pacman -S xf86-video-vmware
+pacman -S xf86-video-vmware
 
 mkinitcpio -p linux
 
-nano /etc/locale.gen  
-uncomment your locale
+vim /etc/locale.gen  
 locale-gen
-
-grub config location : /etc/defaut/grub
 
 mkdir /boot/EFI
 mount efi-partition here
@@ -43,9 +27,4 @@ cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
-systemctl enable gdm
 systemctl enable NetworkManager
-
-exit
-umount -a 
-reboot
