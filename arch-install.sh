599fb5220df0ffba2f7e6c6705535dbf701484fd
shreyanshu-shubham <shreyanshu.dps@gmail.com>
2025-05-16 02:45:28 +0530

    arch-install script

--- arch-install.sh
+++ arch-install.sh
@@ -8,33 +8,26 @@ echo g # Create a new empty DOS partition table
 echo n # Add a new partition
 echo   # Partition number
 echo   # First sector (Accept default: 1)
-echo +1G # Last sector (Accept default: varies)
+echo +1G # Last sector
 # boot PART
 echo n # Add a new partition
 echo   # Partition number
 echo   # First sector (Accept default: 2)
-echo +1G # Last sector (Accept default: varies)
-# swap PART
+echo +1G # Last sector
+# boot PART
 echo n # Add a new partition
 echo   # Partition number
 echo   # First sector (Accept default: 3)
-echo +8G # Last sector (Accept default: varies)
-# root PART
-echo n # Add a new partition
-echo   # Partition number
-echo   # First sector (Accept default: 4)
-echo   # Last sector (Accept default: varies)
+echo   # Last sector
 
 echo w # Write changes
 ) | fdisk /dev/sda
 
 mkfs.fat -F 32 /dev/sda1
 mkfs.ext4 /dev/sda2
-mkswap /dev/sda3
-mkfs.ext4 /dev/sda4
+mkfs.ext4 /dev/sda3
 
-mount /dev/sda4 /mnt
-swapon /dev/sda3
+mount /dev/sda3 /mnt
 mount --mkdir /dev/sda2 /mnt/boot
 mount --mkdir /dev/sda1 /mnt/boot/EFI
 
